252137572f3194dbc819307270012a2235067595
Josue <chipoletoid@gmail.com>
2018-06-17 10:18:24 -0300

    E o projeto Começa.

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Josue
+ *
+ * Created on 16 de Junho de 2018, 22:52
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    return (EXIT_SUCCESS);
+}
+
